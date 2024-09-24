3621f7b7608a5e0e9da338c9b2161fda56217b79
Aayan Aqdas <80333428+aayanaqdas@users.noreply.github.com>
2024-09-24 14:07:12 +0200

    kjør kaklulator

--- front_end.py
+++ front_end.py
@@ -3,11 +3,8 @@ from back_end import *
 
 def printMeny():
     print("------------------- Kalkulator -------------------")
-    print("| 1. Legg sammen (pluss)                         |")
-    print("| 2. Trekk fra   (minus)                         |")
-    print("| 3. Gange       (gange)                         |")
-    print("| 4. Dele        (dele)                          |")
-    print("| 5. Avslutt                                     |")
+    print("| 1. Kalkulator                                  |")
+    print("| 2. Avslutt                                     |")
     print("--------------------------------------------------")
     menyvalg = input("Velg operasjon fra menyen: ")
     utfoerMenyvalg(menyvalg)
@@ -15,25 +12,16 @@ def printMeny():
 
 def utfoerMenyvalg(valgtTall):
     if valgtTall == "1":
-        leggSammen()
+        calculator()
         pause_og_fortsett()
     elif valgtTall == "2":
-        trekkFra()
-        pause_og_fortsett()
-    elif valgtTall == "3":
-        gange()
-        pause_og_fortsett()
-    elif valgtTall == "4":
-        dele()
-        pause_og_fortsett()
-    elif valgtTall == "5":
         bekreftelse = input("Er du sikker på at du vil avslutte? J/N ")
         if (bekreftelse == "J" or bekreftelse == "j"):
             exit()
         else:
             printMeny()
     else:
-        nyttForsoek = input("*** Ugyldig valg. Velg et tall mellom 1-4. Trykk for å fortsette *** ")
+        nyttForsoek = input("*** Ugyldig valg. Velg et tall mellom 1-2. Trykk for å fortsette *** ")
         print(nyttForsoek)
         printMeny()
 
