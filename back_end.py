fd8403819a5f5c6efd7ea093ff527289183981bd
Ludv1k <157578117+Ludv1k@users.noreply.github.com>
2024-09-24 14:02:28 +0200

    Gjorde calculator til en funksjon

--- back_end.py
+++ back_end.py
@@ -1,26 +1,23 @@
-def leggSammen():
-    tall1 = input("Skriv inn det første tallet: ")
-    tall2 = input("Skriv inn det andre tallet: ")
-    sum = int(tall1) + int(tall2)
-    print(f'{tall2} + {tall2} = {sum}')
-
-
-def trekkFra():
-    tall1 = input("Skriv inn det første tallet: ")
-    tall2 = input("Skriv inn det andre tallet: ")
-    diff = int(tall1) - int(tall2)
-    print(f'{tall2} - {tall2} = {diff}')
-
-
-def gange():
-    tall1 = input("Skriv inn det første tallet: ")
-    tall2 = input("Skriv inn det andre tallet: ")
-    sum = int(tall1) * int(tall2)
-    print(f'{tall1} * {tall2} = {sum}')
-
-
-def dele():
-    tall1 = input("Skriv inn det første tallet: ")
-    tall2 = input("Skriv inn det andre tallet: ")
-    sum = int(tall1) / int(tall2)
-    print(f'{tall1} / {tall2} = {sum}')
+def calculator():
+    # Get user input for numbers and operation
+    tall1 = int(input("Skriv inn det første tallet: "))
+    tall2 = int(input("Skriv inn det andre tallet: "))
+    operation = input("Velg operasjon (+, -, *, /): ")
+    # Perform the chosen operation
+    if operation == "+":
+        result = tall1 + tall2
+        print(f'{tall1} + {tall2} = {result}')
+    elif operation == "-":
+        result = tall1 - tall2
+        print(f'{tall1} - {tall2} = {result}')
+    elif operation == "*":
+        result = tall1 * tall2
+        print(f'{tall1} * {tall2} = {result}')
+    elif operation == "/":
+        if tall2 != 0:  # Avoid division by zero
+            result = tall1 / tall2
+            print(f'{tall1} / {tall2} = {result}')
+        else:
+            print("Kan ikke dele med 0")
+    else:
+        print("Ugyldig operasjon. Vennligst prøv igjen.")
